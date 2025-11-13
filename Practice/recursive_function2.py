aa3e9ac41e8395ce27b30ee87aecc6d7f9404043
RMQ1077 <rmq@tutamail.com>
2025-11-13 13:11:43 -0800

    Initial commit

---
+++ Practice/recursive_function2.py
@@ -0,0 +1,18 @@
+'''
+Recursive Function
+'''
+##
+# Define function named factorial 
+# @param n
+# The base case is n <= 1: return 1
+# else @var result = n * factorial(n - 1) This is the recursive call
+# return result
+
+#enter syntax here
+
+
+n = 5
+#Call factorial function and pass n as the argument
+
+#enter syntax here
+
